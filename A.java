450b7367dca5001d6ab51d6980a9bbb450284d61
Relldev メイ <87053694+Rlxfly@users.noreply.github.com>
2021-12-10 13:22:49 +0700

    Create A.java

---
+++ A.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //frame 
+
+        Jframe frame = new Jframe(); //framenya
+        frame.setTittle("Yaha literap");
+        frame.setDefaultCloseOperation(Jframe.EXIT_ON_CLOSE); //tombol close
+        frame.setResizeble(false); //:v
+        frame.setSize(200,200);
+    }
+}
